4a9015d9fe267c7cc68f9d41a2ea72ff9ea86736
lanpai <lan@piyo.cafe>
2021-09-19 07:20:28 -0700

    Update

--- extension/background.js
+++ extension/background.js
@@ -14,55 +14,59 @@ chrome.runtime.onInstalled.addListener(() => {
     title: "Check isAI(Text)",
     contexts: ["selection"],
   });
+});
 
-  chrome.contextMenus.onClicked.addListener(async (clickData) => {
+  chrome.contextMenus.onClicked.addListener((clickData) => {
+    console.log("clicked");
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { to: "loading" });
     });
 
-    let response;
-    let type;
-    if (clickData.mediaType === "image") {
-      // converting "image source" (url) to "blob".
-      let url = clickData.srcUrl;
-      let blob = await (await fetch(url)).blob();
-      var data = new FormData();
-      data.append("data", blob, "file");
-      response = await (
-        await fetch("http://localhost:5000/validate/image", {
-          method: "POST",
-          body: data,
-        })
-      ).json();
-      type = "Image";
-      console.log(response);
-    } else if (clickData.selectionText) {
-      var data = new FormData();
-      data.append("data", clickData.selectionText);
-      response = await (
-        await fetch("http://localhost:5000/validate/text", {
-          method: "POST",
-          body: data,
-        })
-      ).json();
-      type = "Text";
-      console.log(response);
+    async function run() {
+        let response;
+        let type;
+        if (clickData.mediaType === "image") {
+          // converting "image source" (url) to "blob".
+          let url = clickData.srcUrl;
+          let blob = await (await fetch(url)).blob();
+          var data = new FormData();
+          data.append("data", blob, "file");
+          response = await (
+            await fetch("http://localhost:5000/validate/image", {
+              method: "POST",
+              body: data,
+            })
+          ).json();
+          type = "Image";
+        } else if (clickData.selectionText) {
+          var data = new FormData();
+          data.append("data", clickData.selectionText);
+          response = await (
+            await fetch("http://localhost:5000/validate/text", {
+              method: "POST",
+              body: data,
+            })
+          ).json();
+          type = "Text";
+          console.log(response);
+        }
+        else if (clickData.mediaType === "video") {
+          console.log(clickData);
+        }
+        
+        // send the response details to content-script.js to render iframe
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            to: "content-script",
+            prob: response.prob,
+            type,
+            pageUrl: clickData.pageUrl,
+          });
+        });
     }
-    else if (clickData.mediaType === "video") {
-      console.log(clickData);
-    }
-    
-    // send the response details to content-script.js to render iframe
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        to: "content-script",
-        prob: response.prob,
-        type,
-        pageUrl: clickData.pageUrl,
-      });
-    });
+    run();
   });
-});
+//});
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.to === 'add') {
