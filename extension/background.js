00a6ec874983746987538a16a6ec72d1990acd12
Terry Kim <terrykim950@gmail.com>
2021-09-19 02:42:42 -0400

    set up communication between background.js and content-script.js

--- extension/background.js
+++ extension/background.js
@@ -5,34 +5,45 @@ chrome.runtime.onInstalled.addListener(() => {
     contexts: ["image", "video", "selection"],
   });
 
-  chrome.contextMenus.onClicked.addListener(async (clickData, tab) => {
+  chrome.contextMenus.onClicked.addListener(async (clickData) => {
     // console.log(clickData);
     // console.log(tab);
 
-    // if we are checking image file, we need to turn its source url to a file before passing it to the server
+    let response;
+    let type;
     if (clickData.mediaType === "image") {
-      // converting "image source" (url) to "Base64".
+      // converting "image source" (url) to "blob".
       let url = clickData.srcUrl;
       let blob = await (await fetch(url)).blob();
       var data = new FormData();
       data.append("data", blob, "file");
-      const response = await (
+      response = await (
         await fetch("http://localhost:5000/validate/image", {
           method: "POST",
           body: data,
         })
       ).json();
+      type = "image";
       console.log(response);
     } else if (clickData.selectionText) {
       var data = new FormData();
       data.append("data", clickData.selectionText);
-      const response = await (
+      response = await (
         await fetch("http://localhost:5000/validate/text", {
           method: "POST",
           body: data,
         })
       ).json();
+      type = "text";
       console.log(response);
     }
+    // send the response details to content-script.js to render iframe
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        to: "content-script",
+        prob: response.prob,
+        type,
+      });
+    });
   });
 });
--- extension/content-script.js
+++ extension/content-script.js
@@ -1,1 +1,8 @@
 console.log("content-script run");
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.to === "content-script") {
+    // render the iframe with details from background.js
+    console.log("received probability", request.prob);
+    console.log("data type: ", request.type);
+  }
+});
