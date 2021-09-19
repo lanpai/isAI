0cb8b80a5cc094536d591bc3310eaebea6c9f32d
lanpai <lan@piyo.cafe>
2021-09-18 21:27:43 -0700

    Using TenCrop

--- backend/image_validation.py
+++ backend/image_validation.py
@@ -15,15 +15,20 @@ model.load_state_dict(state_dict['model'])
 model.eval()
 
 trans = transforms.Compose([
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-
+    transforms.TenCrop(224),
+    transforms.Lambda(lambda images: torch.stack([
+        transforms.ToTensor()(image)
+        for image in images ])),
+    transforms.Lambda(lambda images: torch.stack([
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(image)
+        for image in images ]))])
 
 def validateImage(img):
-    img = trans(Image.open(img).convert('RGB'))
+    transImages = trans(Image.open(img).convert('RGB'))
 
     with torch.no_grad():
-        in_tens = img.unsqueeze(0)
+        in_tens = transImages.unsqueeze(0)
         # in_tens = in_tens.cuda()
-        return model(in_tens).sigmoid().item()
+        bs, ncrops, c, h, w = in_tens.size()
+        res = model(in_tens.view(-1, c, h, w))
+        return res.view(bs, ncrops, -1).mean(1).sigmoid().item()
